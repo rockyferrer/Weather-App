436af3fa993c41e94809bcce0611cb4b4e74c393
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-07 23:16:30 -0400

    updated repo for heroku

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ var express = require('express');
 // Create the app
 
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function(req, res, next) {
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        next();
+    } else { //redirect to http
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
+
 
 app.use(express.static('public'));
 
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000; press Ctrl-C to terminate');
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT '; press Ctrl-C to terminate');
 });
