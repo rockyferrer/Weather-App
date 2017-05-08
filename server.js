4503a403915e9871021524a9004be6a2231177b4
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-08 10:50:36 -0400

    changed redirect

--- server.js
+++ server.js
@@ -7,9 +7,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(function(req, res, next) {
     if (req.headers['x-forwarded-proto'] === 'https') {
-        res.redirect('http://' + req.hostname + req.url);
-    } else {
         next();
+    } else {
+        res.redirect('http://' + req.hostname + req.url);
     }
 });
 
