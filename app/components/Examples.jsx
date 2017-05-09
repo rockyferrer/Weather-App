41ad1940218fbf10beccd4d5767d6159fd460aee
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-09 16:02:59 -0400

    added styles to main component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few example cities to try out</p>
+            <ol>
+                <li>
+                    <Link to='/?city=Toronto'>Toronto, ON</Link>
+                </li>
+                <li>
+                    <Link to='/?city=Manila'>Manila, Philippines</Link>
+                </li>
+            </ol>
         </div>
     );
 }
