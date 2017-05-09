4a289cf6c578921a53cb51362994139625961ebb
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-09 16:18:44 -0400

    added styles to about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,30 @@ var React = require('react');
 var About = (props) => {
     return (
         <div>
-            <h3>About</h3>
-            <p>Welcome to About page</p>
+            <h1 className="text-center">About</h1>
+            <p>
+                This is a simple weather application built on React
+            </p>
+            <p>
+                Tools used:
+            </p>
+            <ul>
+                <li>
+                    <a href="https://facebook.github.io/react">React</a> - JavaScript framework used
+                </li>
+                <li>
+                    <a href="http://openweathermap.org">Open Weather Map</a> - API for weather data
+                </li>
+                <li>
+                    <a href="http://foundation.zurb.com/">Foundation</a> - CSS framework used
+                </li>
+                <li>
+                    <a href="https://webpack.github.io/">Webpack</a> - Module bundler
+                </li>
+                <li>
+                    <a href="https://www.heroku.com/">Heroku</a> - Deployment
+                </li>
+            </ul>
         </div>
     );
 }
