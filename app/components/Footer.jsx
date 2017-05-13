831f89cd1665e58b4267509d46727ea30b87931a
rockyferrer <rocky.ferrer@mail.utoronto.ca>
2017-05-13 00:25:54 -0400

    added footer component

---
+++ app/components/Footer.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Footer = React.createClass({
+    render: function() {
+        return (
+            <div id="footer">
+                © 2017 Rocky Ferrer
+            </div>
+        );
+    }
+});
+
+module.exports = Footer;
